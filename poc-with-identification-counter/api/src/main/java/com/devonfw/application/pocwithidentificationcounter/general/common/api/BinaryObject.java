7b206fc96a34134b501c5cc8bffe0a7814091cb5
Cristian <cristian.trives@capgemini.com>
2019-05-10 12:26:27 +0200

    Initial poc -with-identification-counter commit

---
+++ poc-with-identification-counter/api/src/main/java/com/devonfw/application/pocwithidentificationcounter/general/common/api/BinaryObject.java
@@ -0,0 +1,32 @@
+package com.devonfw.application.pocwithidentificationcounter.general.common.api;
+
+/**
+ * This is the interface for a {@link BinaryObject} of the poc-with-identification-counter.
+ */
+public interface BinaryObject extends ApplicationEntity {
+
+  /**
+   * @param mimeType is the MIME-Type of this {@link BinaryObject}
+   */
+  void setMimeType(String mimeType);
+
+  /**
+   * Returns MIME-Type of thie {@link BinaryObject}
+   *
+   * @return the MIME-Type, e.g image/jpeg
+   */
+  String getMimeType();
+
+  /**
+   * @return Returns the size in bytes
+   */
+  long getSize();
+
+  /**
+   * Sets the size of bytes
+   *
+   * @param size the size in bytes
+   */
+  void setSize(long size);
+
+}
