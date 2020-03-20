7d6d5631fe8cd28b05cadb40c92c689d107c07fc
Nair <sujith.nair@capgemini.com>
2020-03-20 15:11:36 +0530

    DEVC-882 removed id from the SearchCriteriaTo, id can be used seaparately for search not required to be part of the other fields

--- poc-with-identification-counter/api/src/main/java/com/devonfw/application/pocwithidentificationcounter/employeemanagement/logic/api/to/EmployeeSearchCriteriaTo.java
+++ poc-with-identification-counter/api/src/main/java/com/devonfw/application/pocwithidentificationcounter/employeemanagement/logic/api/to/EmployeeSearchCriteriaTo.java
@@ -1,6 +1,5 @@
 package com.devonfw.application.pocwithidentificationcounter.employeemanagement.logic.api.to;
 
-import com.devonfw.application.pocwithidentificationcounter.employeemanagement.common.api.CompositeEmployeeKey;
 import com.devonfw.application.pocwithidentificationcounter.general.common.api.to.AbstractSearchCriteriaTo;
 import com.devonfw.module.basic.common.api.query.StringSearchConfigTo;
 
@@ -10,92 +9,78 @@ import com.devonfw.module.basic.common.api.query.StringSearchConfigTo;
  */
 public class EmployeeSearchCriteriaTo extends AbstractSearchCriteriaTo {
 
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	private String name;
+  private String name;
 
-	private String lastName;
+  private String lastName;
 
-	private StringSearchConfigTo nameOption;
+  private StringSearchConfigTo nameOption;
 
-	private StringSearchConfigTo lastNameOption;
+  private StringSearchConfigTo lastNameOption;
 
-	private CompositeEmployeeKey id;
+  /**
+   * @return nameId
+   */
+  public String getName() {
 
-	/**
-	 * @return nameId
-	 */
-	public String getName() {
-		return name;
-	}
+    return this.name;
+  }
 
-	/**
-	 * @param name setter for name attribute
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
+  /**
+   * @param name setter for name attribute
+   */
+  public void setName(String name) {
 
-	/**
-	 * @return lastNameId
-	 */
-	public String getLastName() {
-		return lastName;
-	}
+    this.name = name;
+  }
 
-	/**
-	 * @param lastName setter for lastName attribute
-	 */
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+  /**
+   * @return lastNameId
+   */
+  public String getLastName() {
 
-	/**
-	 * @return the {@link StringSearchConfigTo} used to search for {@link #getName()
-	 *         name}.
-	 */
-	public StringSearchConfigTo getNameOption() {
+    return this.lastName;
+  }
 
-		return this.nameOption;
-	}
+  /**
+   * @param lastName setter for lastName attribute
+   */
+  public void setLastName(String lastName) {
 
-	/**
-	 * @param nameOption new value of {@link #getNameOption()}.
-	 */
-	public void setNameOption(StringSearchConfigTo nameOption) {
+    this.lastName = lastName;
+  }
 
-		this.nameOption = nameOption;
-	}
+  /**
+   * @return the {@link StringSearchConfigTo} used to search for {@link #getName() name}.
+   */
+  public StringSearchConfigTo getNameOption() {
 
-	/**
-	 * @return the {@link StringSearchConfigTo} used to search for
-	 *         {@link #getLastName() lastName}.
-	 */
-	public StringSearchConfigTo getLastNameOption() {
+    return this.nameOption;
+  }
 
-		return this.lastNameOption;
-	}
+  /**
+   * @param nameOption new value of {@link #getNameOption()}.
+   */
+  public void setNameOption(StringSearchConfigTo nameOption) {
 
-	/**
-	 * @param lastNameOption new value of {@link #getLastNameOption()}.
-	 */
-	public void setLastNameOption(StringSearchConfigTo lastNameOption) {
+    this.nameOption = nameOption;
+  }
 
-		this.lastNameOption = lastNameOption;
-	}
+  /**
+   * @return the {@link StringSearchConfigTo} used to search for {@link #getLastName() lastName}.
+   */
+  public StringSearchConfigTo getLastNameOption() {
 
-	/**
-	 * @return idId
-	 */
-	public CompositeEmployeeKey getId() {
-		return id;
-	}
+    return this.lastNameOption;
+  }
 
-	/**
-	 * @param id setter for id attribute
-	 */
-	public void setId(CompositeEmployeeKey id) {
-		this.id = id;
-	}
+  /**
+   * @param lastNameOption new value of {@link #getLastNameOption()}.
+   */
+  public void setLastNameOption(StringSearchConfigTo lastNameOption) {
+
+    this.lastNameOption = lastNameOption;
+  }
 
 }
